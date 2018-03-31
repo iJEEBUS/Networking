58d3b2ce8668d20709662c0c27d926c2af60b24b
Ronald Rounsifer <ronrounsifer@me.com>
2018-03-30 21:54:09 -0400

    UDP client added.

--- UDPclient.py
+++ UDPclient.py
@@ -1,18 +1,17 @@
 import socket
 
-target_host = "www.google.com"
+target_host = "127.0.0.1"
 target_port = 80
 
 # create a socket object
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # add the option to reuse the socket later
 client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# connect the client
 client.bind((target_host, target_port))
 
-message = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+message = "This is an initial test for the UDP client."
 # send data
 client.sendto(message.encode(), (target_host,target_port))
 
@@ -20,4 +19,4 @@ client.sendto(message.encode(), (target_host,target_port))
 response, address = client.recvfrom(4096)
 
 #print data
-print(response)
+print(response, address)
